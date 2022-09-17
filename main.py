6aa7e862d4bee6402aa75902be2a0a1c63499cea
MarisQuinte014 <78067788+MarisQuinte014@users.noreply.github.com>
2022-09-16 20:19:24 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print(f'---Digita 1 para recibir el nombre y año de nacimiento---')
+print(f'---Digita 2 para calcular la edad y mostrar en la pantalla---')
+print(f'---Digita 3 para mostrar saludo de buenas noches al usuario---')
+print(f'---Digita 4 para contarle cuantos años tendra en el 2030---')
+print(f'---Digita 5 para Salir---')
+
+opcion = 100
+
+while opcion != 5:
